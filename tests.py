509b7fab8e6ff4ddc6f273d81e88c5c6f7dfbf39
All Eyes <matthewacha@gmail.com>
2018-02-20 15:11:55 +0300

    ft-tests-tests for business api enpoints

--- tests.py
+++ tests.py
@@ -2,7 +2,9 @@ import os
 import unittest
 import json
 from app import app, users
-from app.models import User, Business
+
+
+    
 
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
@@ -50,6 +52,95 @@ class TestUserApi(BaseTestCase):
         data = json.loads(response.data.decode())
         self.assertTrue(data['token'])
         self.assertEqual(response.status_code, 200)
+
+    #ensure user_token generated on login
+    def test_token_generate(self):
+        self.tester.post('/api/user',content_type='application/json',
+                                   data =json.dumps( dict(
+                                                        email='jh@gmail.com',
+                                                        password='laters')))
+        response = self.tester.post('/login',
+                                    content_type='application/json',
+                                   data=json.dumps(dict(email='jh@gmail.com',
+                                                      password='laters')))
+        data = json.loads(response.data.decode())
+        self.assertTrue(data['token'])
+        self.assertEqual(response.status_code, 200)
+
+        #################
+        ##TEST BUSINESS##
+        #################
+
+    def test_register_business(self):
+        self.tester.post('/api/user',content_type='application/json',
+                                   data =json.dumps( dict(email='jh@gmail.com',
+                                                        password='amazons')))
+        login = self.tester.post('/login',
+                                 content_type= 'application/json',
+                                 data=json.dumps(dict(email='jh@gmail.com',
+                                                      password='amazons')))
+        
+        result = json.loads(login.data.decode())
+        response = self.tester.post('/api/business',content_type='application/json',
+                                   data =json.dumps( dict(name='Fish To Go',
+                                                        description='We sell fishy stuff')),
+                                    headers =dict(a_access_token = result))#pragma:no cover
+        #data = json.loads(response.data.decode())#pragma:no cover
+        self.assertIn(u'Successfully added business',response.data)#pragma:no cover
+        self.assertEqual(response.status_code, 200)
+
+    #ensure businesses can be viewed publicly
+    def test_view_businesses(self):
+        self.tester.post('/api/user',content_type='application/json',
+                                   data =json.dumps( dict(
+                                                        email='jh@gmail.com',
+                                                        password='amazon')))
+        user_login = self.tester.post('/login',content_type='application/json',
+                         data=json.dumps(dict(email='jh@gmail.com',password='amazon')))
+
+        result = json.loads(user_login.data.decode())
+        self.tester.post('/api/business',content_type='application/json',
+                                   data =json.dumps( dict(name='Restaurant',
+                                                        description='We cook')),
+                         headers =dict(access_token=result))
+        self.tester.post('/api/business',content_type='application/json',
+                                   data =json.dumps( dict(name='School',
+                                                        description='We teach')),
+                         headers=dict(access_token=result))
+        response = self.tester.get('/api/business',
+                                  content_type='application/json')
+        #data = json.loads(response.data.decode())
+        self.assertIn(u'School', response.data)
+        self.assertEqual(response.status_code, 200)
+
+    #ensure single business can be viewed
+    def test_view_single_business(self):
+        self.tester.post('/api/user',content_type='application/json',
+                                   data =json.dumps( dict(
+                                                        email='jh@gmail.com',
+                                                        password='amazon')))
+        user_login = self.tester.post('/login',content_type='application/json',
+                         data=json.dumps(dict(email='jh@gmail.com',password='amazon')))
+        result = json.loads(user_login.data.decode())
+        self.tester.post('/api/business',content_type='application/json',
+                                   data =json.dumps( dict(name='School',
+                                                        description='We teach')),
+                         headers =dict(access_token=result))
+        self.tester.post('/api/business',content_type='application/json',
+                                   data =json.dumps( dict(name='Restaurant',
+                                                        description='We cook')),
+                         headers =dict(access_token=result))
+        self.tester.post('/api/business',content_type='application/json',
+                                   data =json.dumps( dict(name='Bookshop',
+                                                        description='We read')),
+                         headers =dict(access_token=result))
+        response = self.tester.get('/api/recipe/<name>',
+                                   content_type='application/json',
+                                   data = json.dumps(dict(name='School')))
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(u'We teach', response.data)
+
 
 if __name__ == "__main__":
     unittest.main()
