5f688297587386afd89c3bf15e1607298d5a8f89
All Eyes <matthewacha@gmail.com>
2018-02-21 07:11:32 +0300

    bg-login-fix bug in login endpoint

--- app/users/views.py
+++ app/users/views.py
@@ -2,7 +2,7 @@ import os
 import random
 import jwt
 import datetime
-from flask import Flask, jsonify, request, session, make_response
+from flask import Flask, jsonify, request, session, make_response, abort
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 from . import users
@@ -19,6 +19,34 @@ def generate_id():
         id = int(id)
         return id
 
+class User(object):
+        def __init__(self, email, password):
+                self.email = email
+                self.password = password
+
+        def change_password(self, new_password):
+                password = self.new_password
+                return password
+        def email(self):
+                return self.email
+        def password(self):
+                return self.password
+
+class Business(object):
+        def __init__(self, name, location, category):
+                self.name = name
+                self.location = location
+                self.category = category
+
+        def change_name(self,new_name):
+                name = self.new_name
+                return name
+        def change_location(self,new_location):
+                location = self.location
+                return location
+        def change_category(self,new_category):
+                category = self.new_category
+
 def token_required(funct):
     @wraps(funct)
     def decorated_funct(*args, **kwargs):
@@ -29,6 +57,15 @@ def token_required(funct):
                 return jsonify({"message":"Token is missing"}), 401#pragma:no cover
             try:
                 data = jwt.decode(token, secret_key)
+                user_profiles = []
+                for user in database:
+                        user_profiles.append({'email':user['details'].email,
+                                  'password':user['details'].password,
+                                  'user_id':user['id'],
+                                  'businesses':user['businesses']})
+                for user in user_profiles:
+                        if user['user_id'] == data['sub']:
+                                current_user = user
             except:
                 return jsonify({"message":"Token is invalid"}), 401
             return funct(current_user, *args, **kwargs)
@@ -37,9 +74,10 @@ def token_required(funct):
 @users.route('/api/auth/register', methods = ['POST'])
 def add_user():
     json_data = request.get_json()
-    user_profile = {'email': json_data['email'],
-                    'password':generate_password_hash(json_data['password']),
-                    'id':generate_id(),                
+
+    user = User(json_data['email'], generate_password_hash(json_data['password']))
+    user_profile = {'details':user,
+                    'user_id':generate_id(),
                     'businesses':[]}
     database.append(user_profile)
     message = 'Successfully signed up'
@@ -49,21 +87,33 @@ def add_user():
 
 @users.route('/api/auth/login', methods = ['POST'])
 def login():
-    auth = request.get_json()
+        auth = request.get_json()
+        if not auth or not auth['email'] or not auth['password']:
+                return make_response(("Authorize with email and password"), 401)
 
-    if not auth or not auth['email'] or not auth['password']:
-        return make_response(("Authorize with email and password"), 401)
-    
-    for user in database:
-        if user['email'] == auth['email']:
-            user = user
+        user_profiles = []
+        for user in database:
+                user_profiles.append({'email':user['details'].email,
+                                  'password':user['details'].password,
+                                  'user_id':user['user_id'],
+                                  'businesses':user['businesses']})
 
-        #user = user
+        user = [user for user in user_profiles if user['email'] == auth['email']]
+        if len(user) == 0 :
+                return make_response(("Authorize with correct credentials"), 401)
+        info = user[0]
+        if check_password_hash(info['password'], auth['password']):
+                token = jwt.encode({
+                        "exp": datetime.datetime.utcnow() + datetime.timedelta(days = 0, minutes = 45),
+                        "iat": datetime.datetime.utcnow(),
+                        "sub": info['user_id']}, secret_key, algorithm = 'HS256')
+                return jsonify({'token':token})
+        return make_response(("Authorize with correct password"), 401)
+
+@users.route('/api/auth/logout', methods = ['POST'])
+def logout():
+        if 'access_token' in request.headers:
+            token = request.headers['access_token']
+            token = None
         
-    if check_password_hash(user['password'], auth['password']):
-        token = jwt.encode({
-            "exp": datetime.datetime.utcnow() + datetime.timedelta(days = 0, minutes = 45),
-            "iat": datetime.datetime.utcnow(),
-            "sub": user['id']}, secret_key, algorithm = 'HS256')
-        return jsonify({'token':token})
-    return make_response(("Authorize with correct password"), 401)
+        return make_response(("Successfully logged out"), 200)
