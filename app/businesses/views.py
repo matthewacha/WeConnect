1dee430853fa3ca0a862e149e6b14b989ac83ed0
All Eyes <matthewacha@gmail.com>
2018-02-21 15:09:51 +0300

    ft-businesses endpoint-add register business endpoint

--- app/businesses/views.py
+++ app/businesses/views.py
@@ -1,21 +1,51 @@
 import os
 import random
+import jwt
 from flask import Flask, jsonify, request, session, make_response, abort
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.users.views import database, Business, generate_id, token_required
+from functools import wraps
 from . import businesses
 from json import dumps
 
-business_data = []
-
+secret_key = "its_so_secret_1945"
+businesses_db = []
 @businesses.route('/api/businesses', methods = ['POST'])
-def register():
+@token_required
+def register(current_user):
     data = request.get_json()
-    business = Business(data['name'],data['description'], data['location'], data['category'])
+    business = Business(data['name'],data['description'], data['location'], data['category'],user_id=current_user['user_id'])
     biz_profile = {"details":business,
-                   "biz_id":generate_id,
+                   "biz_id":generate_id(),
                    "reviews":[]}
-    business_data.append(biz_profile)
-    message = "Successfully added business"
+    businesses_db.append(biz_profile)
+    
+    return jsonify({'message':"Successfully added business"})
 
+"""
+@businesses.route('/api/businesses', methods = ['GET'])
+@token_required
+def retrieve(current_user):
+    businesses = [user['businesses'] for user in database if user['details'].email == current_user['details'].email]
+
+    return jsonify({"businesses":businesses})
+
+
+@businesses.route('/api/businesses/<businessId>', methods = ['GET'])
+@token_required
+def retrieve_business(current_user,businessId):
+    business = 'business'
+    return jsonify({"business":business})
+
+@businesses.route('/api/businesses/<businessId>/reviews', methods = ['POST'])
+@token_required
+def post_review(current_user, businessId):
+    message = "message"
     return jsonify({"message":message})
+
+@businesses.route('/api/businesses/<businessId>', methods = ['GET'])
+@token_required
+def get_reviews(current_user, businessId):
+    reviews = ""
+    return jsonify({"reviews":reviews})
+"""
