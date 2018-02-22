f971b09c15a25a025a9c67047669908c983dff62
All Eyes <matthewacha@gmail.com>
2018-02-22 07:41:38 +0300

    ft-tests-Add tests for review endpoint

--- tests.py
+++ tests.py
@@ -233,6 +233,42 @@ class TestUserApi(BaseTestCase):
         self.assertIn(u'Successfully added review', response.data)
         self.assertEqual(response.status_code, 200)
 
+    #ensure reviews can be viewed for business
+    def test_view_reviews(self):
+        self.tester.post('/api/auth/register',content_type='application/json',
+                                   data =json.dumps( dict(
+                                                        email='jh@gmail.com',
+                                                        password='amazon')))
+        user_login = self.tester.post('/api/auth/login',content_type='application/json',
+                         data=json.dumps(dict(email='jh@gmail.com',password='amazon')))
+
+        result = json.loads(user_login.data.decode())
+        self.tester.post('/api/businesses',content_type='application/json',
+                         data = json.dumps( dict(name='Restaurant',
+                                                 description='We cook',
+                                                 location = 'Kampala',
+                                                 category = "Food")),
+                         headers = dict(access_token=result['token']))
+        self.tester.post('/api/businesses/Restaurant/reviews',
+                                    content_type = 'application/json',
+                                    data = json.dumps(dict(review = "It's awesome")),
+                                    headers = dict(access_token = result['token']))
+        self.tester.post('/api/businesses/Restaurant/reviews',
+                                    content_type = 'application/json',
+                                    data = json.dumps(dict(review = "Amazing place")),
+                                    headers = dict(access_token = result['token']))
+        self.tester.post('/api/businesses/Restaurant/reviews',
+                                    content_type = 'application/json',
+                                    data = json.dumps(dict(review = "Chef Rico is the best!")),
+                                    headers = dict(access_token = result['token']))
+        
+        response = self.tester.get('/api/businesses/Restaurant/reviews',
+                                    content_type = 'application/json',
+                                    headers = dict(access_token = result['token']))
+        #data = json.loads(response.data.decode())
+        self.assertIn(u'Amazing place', response.data)
+        self.assertEqual(response.status_code, 200)
+
     #ensure business can be edited by logged in user
     def test_edit_business(self):
         self.tester.post('/api/auth/register',content_type='application/json',
