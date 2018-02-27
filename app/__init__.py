51333db5fddb5ad56e4d43191c94115dadb37500
unknown <matthewacha@gmail.com>
2018-02-28 01:27:06 +0300

    Change config

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config.from_object('config.Development')
+app.config.from_object('config.Deployment')
 
 db = SQLAlchemy(app)
 db.init_app(app)
