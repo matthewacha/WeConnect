5c88355fffbe155edbfac70891ca17df1a60f147
unknown <matthewacha@gmail.com>
2018-03-01 21:10:06 +0300

    bg-Fix bugs in delete, update, review endpoints

--- app/v1/businesses/views.py
+++ app/v1/businesses/views.py
@@ -4,7 +4,7 @@ import jwt
 from flask import Flask, jsonify, request, session, make_response, abort
 from flasgger.utils import swag_from
 from werkzeug.security import generate_password_hash, check_password_hash
-from app.v1.models import database, Business, generate_id
+from app.v1.models import database, Business, generate_bizId, generate_id
 from app.v1.users.views import token_required
 from functools import wraps
 from . import businessesv1
@@ -18,19 +18,24 @@ businesses_db = []
 @token_required
 def register(current_user):
     data = request.get_json()
-    business = Business(data['name'],data['description'], data['location'], data['category'],user_id=current_user['user_id'])
+    for business in businesses_db:
+        if data['name']==business['details'].name:
+            return make_response(('Business already exists, try another'),401)
+    business = Business(data['name'],data['description'], data['location'], data['category'],current_user['user_id'])
     biz_profile = {"details":business,
-                   "biz_id":generate_id(),
+                   "biz_id":generate_bizId(businesses_db),
                    "reviews":[]}
     businesses_db.append(biz_profile)
     
     return jsonify({'message':"Successfully added business"})
 
 
-
-@businessesv1.route('/businesses/<name>', methods = ['GET'])
+@businessesv1.route('/businesses/<businessId>', methods = ['GET'])
 @swag_from('../api-docs/v1/view_business.yml')
-def retrieve_business(name):
+@token_required
+def retrieve_business(current_user, businessId):
+    Idlist = map(int, businessId.split())
+    businessId = Idlist[0]
     output = []
     for business in businesses_db:
         business_={
@@ -46,18 +51,19 @@ def retrieve_business(name):
     business_out = []
 
     for business in output:
-        if business["name"] == name:
+        if business["businessId"] == businessId:
             business_out.append(business)
 
     if len(business_out) == 0:
         message = ["Business does not exist"]
     else:
         message = business_out
-    return jsonify({"business":message[0]})
+    return jsonify({"business":message})
 
 @businessesv1.route('/businesses', methods = ['GET'])
 @swag_from('../api-docs/v1/view_businesses.yml')
-def retrieve_businesses():
+@token_required
+def retrieve_businesses(current_user):
     output = []
     for business in businesses_db:
         business_={
@@ -77,15 +83,16 @@ def retrieve_businesses():
 
     return jsonify({"businesses":message})
 
-@businessesv1.route('/businesses/<name>', methods = ['PUT'])
+@businessesv1.route('/businesses/<businessId>', methods = ['PUT'])
 @swag_from('../api-docs/v1/update_business.yml')
 @token_required
-def edit_business(current_user, name):
+def edit_business(current_user, businessId):
+    Idlist = map(int, businessId.split())
+    businessId = Idlist[0]
     data = request.get_json()
-    
     for business in businesses_db:
         if business['details'].user_id == current_user['user_id']:
-            if business['details'].name == name:
+            if business['biz_id'] == businessId:
                 if data['new_name']:
                     business['details'].name = data['new_name']
                 if data['new_description']:
@@ -94,47 +101,54 @@ def edit_business(current_user, name):
                     business['details'].category = data['new_category']
                     message = "Successfully edited"
             else:
-                message = "Business does not exist"
+                return make_response(("Business does not exists"), 401)
         else:
-            message = "You do not have authorization"
+            return make_response(("Authoriation required"),401)
                 
     return jsonify({"message":message })
 
-@businessesv1.route('/businesses/<name>', methods = ['DELETE'])
+@businessesv1.route('/businesses/<businessId>', methods = ['DELETE'])
 @swag_from('../api-docs/v1/delete_business.yml')
 @token_required
-def delete_business(current_user, name):
+def delete_business(current_user, businessId):
+    Idlist = map(int, businessId.split())
+    businessId = Idlist[0]
     for business in businesses_db:
         if business['details'].user_id == current_user['user_id']:
-            if business["details"].name == name:
+            if business["biz_id"] == businessId:
                 businesses_db.remove(business)
                 message = "Successfully deleted"
             else:
-                message = "Business does not exist"
+                return jsonify({"message":[businessId,business["biz_id"]]})
+                #message = "Business does not exist"
         else:
-            message = "You are not authorized"
+            return make_response(("You are not authorized"), 401)
     return jsonify({"message":message})
 
-@businessesv1.route('/businesses/<name>/reviews', methods = ['POST'])
+@businessesv1.route('/businesses/<businessId>/reviews', methods = ['POST'])
 @swag_from('../api-docs/v1/post_review.yml')
 @token_required
-def post_review(current_user, name):
+def post_review(current_user, businessId):
+    Idlist = map(int, businessId.split())
+    businessId = Idlist[0]
     data = request.get_json()
     review = data['review']
     for business in businesses_db:
-        if business["details"].name == name:
+        if business["biz_id"] == businessId:
             business['reviews'].append(review)
             message = "Successfully added review"
     return jsonify({"message":message})
 
 
-@businessesv1.route('/businesses/<name>/reviews', methods = ['GET'])
+@businessesv1.route('/businesses/<businessId>/reviews', methods = ['GET'])
 @swag_from('../api-docs/v1/view_reviews.yml')
 @token_required
-def view_reviews(current_user, name):
+def view_reviews(current_user, businessId):
+    Idlist = map(int, businessId.split())
+    businessId = Idlist[0]
     all_reviews = []
     for business in businesses_db:
-        if business['details'].name == name:
+        if business["biz_id"] == businessId:
             all_reviews = business['reviews']
     if len(all_reviews)==0:
         message="No reviews"
